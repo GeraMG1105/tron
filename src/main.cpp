49ad15946ae5a68718ef352df77ffff442c4797c
GeraMG1105 <160290048+GeraMG1105@users.noreply.github.com>
2024-04-23 01:44:24 +0000

    v2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/dom/elements.hpp>
-#include <thread> //Hilos de procesos
+#include <thread>  //Hilos de procesos
 #include <fstream> // COntrol de cadenas
-#include <string> // control de cadenas
-
+#include <string>  // control de cadenas
+#include <dibujo.hpp>
 
 using namespace std;
 using namespace ftxui;
@@ -12,52 +12,21 @@ using namespace ftxui;
 int main(int argc, char const *argv[])
 
 {
-    this_thread::sleep_for(0.1s);
-
-    ifstream archivo;
-    archivo.open("assets/images/moto.txt");
-    
-    Elements elementos;
-    string linea;
-    // Funcion para imprimir moto
-        while (getline(archivo,linea))
-        {
-            elementos.emplace_back(text(linea));
-        }
-        Element moto=vbox(elementos);
-        archivo.close();
-    
-    
-    // Funcion para imprimir todo el bloque
-        string linea2;
-        ifstream archivo2;
-        archivo2.open("assets/images/bloque.txt");
-        while (getline(archivo2, linea2))
-        {
-            elementos.emplace_back(text(linea2));
-
-        }
-        Element bloque=vbox(elementos);
-        archivo2.close();
-
-    
-
-
+    Dibujo moto("./assets/images/moto.txt");
+    Dibujo bloque("./assets/images/bloque.txt");
 
     int fotograma = 0;
     while (true)
     {
-    fotograma++;
-    Element personaje = spinner(21,fotograma) | bold | color(Color::Orange1) | bgcolor(Color::Black);
-    //declaracion 
-    Element dibujo = hbox({personaje, moto, bloque}); 
-    Screen pantalla = Screen::Create(Dimension::Full());
-    Render(pantalla,dibujo);
-    pantalla.Print();
-    cout<<pantalla.ResetPosition();
+        fotograma++;
+        Element personaje = spinner(21, fotograma) | bold | color(Color::Orange1) | bgcolor(Color::Black);
+        // declaracion
+        Element dibujo = vbox({personaje, moto.GetElement(), bloque.GetElement()});
+        Screen pantalla = Screen::Create(Dimension::Full());
+        Render(pantalla, dibujo);
+        pantalla.Print();
+        cout << pantalla.ResetPosition();
     }
-    
 
     return EXIT_SUCCESS;
-    
 }
